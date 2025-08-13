6b891051a26ea6bfa96a339f441e8a7296bda745
Josue <jowebdesigc@gmail.com>
2025-08-13 11:10:35 -0400

    sasd

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
